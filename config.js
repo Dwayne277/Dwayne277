3160298bda93670a712a50a3d7930819d8d1691d
markza <kipronom130@gmail.com>
2025-03-20 11:21:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    autoLikeStatus: true,
+    downloadMediaStatus: false,
+    sensorNumber: true,
+    sendWelcomeMessage: true,
+    blackList: [],
+    whiteList: [],
+    ownerNumber: '254724394091',
+    userTimezone: 'Asia/Colombo',
+    mongoUrl: 'mongodb+srv://Public:Public@cluster0.aahbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName: 'Public', // Replace with your database name
+    botDetails: {
+        botName: 'MARKZA',
+        botAge: '16',
+        botLocation: 'mathara',
+        fenixSaveName: '𝐍ɑ͢ɱꪸ𝛆'
+    },
+    relaxWishes: [
+        "Take a deep breath... 🌿",
+        "Relax, we’ve got this... 🌸",
+        "Sit back, it’s almost ready... 🌙",
+        "Calmly, we’re setting it up... 🌟",
+        "Take a moment to unwind... 🌼",
+        "Everything is coming together... ✨",
+        "Rest easy, we’re on it... 🌺",
+        "Breathe in, breathe out... 🌱",
+        "Let go and relax... 🌙",
+        "Good things are on the way... 🌻"
+    ]
+};
